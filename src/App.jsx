870cc948dcba0a24177d9c2cd8432e3c1f5ffdb2
Chesney Julian <chesney.julian724@gmail.com>
2023-09-06 14:34:28 -0400

    add header and navigation basic rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-
-
+import Header from './components/Header'
 import './App.css'
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-medium">Chesney Julian</h1>
+    <>
+    <main>
+      <Header />
+    </main>
+    </>
   )
 }
 
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,32 @@
+const navItems = [
+  {
+    name: 'About Me',
+    href: '#'
+  },
+  {
+    name: 'Recent Projects',
+    href: '#'
+  },
+  {
+    name: 'Contact Me',
+    href: '#'
+  },
+  {
+    name: 'Resume',
+    href: '#'
+  },
+]
+
+function Navigation () {
+  return (
+    <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
+    <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2 md:flex lg:gap-x-10">
+      {navItems.map((item) => (
+        <a key={item.name} href={item.href}>{item.name}</a>
+      ))}
+    </div>
+  </div>
+  )
+}
+
+export default Navigation;
