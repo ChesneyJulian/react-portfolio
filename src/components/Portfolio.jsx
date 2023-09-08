ba2ec61ebdc7e12caaffbf2c7d263178166d9764
Chesney Julian <chesney.julian724@gmail.com>
2023-09-08 17:56:38 -0400

    add responsive styling to grid cols in portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -6,7 +6,7 @@ export default function Portfolio() {
       <div className='mb-12'>  
         <h3 className='text-3xl font-semibold'>Portfolio</h3>
       </div>
-      <div className='grid grid-cols-3'>
+      <div className='grid grid-cols-1 md:grid-cols-3'>
        <Project />
       </div>
     </>
