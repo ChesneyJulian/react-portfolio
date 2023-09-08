011f3105cb122e7ab8400e9763de16de23537afd
Chesney Julian <chesney.julian724@gmail.com>
2023-09-08 16:37:34 -0400

    remove event.preventDefault and change type to download for resume download button

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -5,8 +5,7 @@ function Resume() {
     <div>
       <a href={resumePdf} download='Chesney-Julian-Resume.pdf' className=''>
         <button
-        type="submit"
-        onClick={(e) => e.preventDefault()}
+        type="download"
         className="py-2 px-4 bg-[#057d69] hover:bg-[#05ab8f] rounded my-8" >
           Download Resume
         </button>
