c167bc13684356454c7f94d410f728cd8378a5be
Chesney Julian <chesney.julian724@gmail.com>
2023-09-11 19:13:11 -0400

    adjust grid cols for md screens in portfolio component

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -7,7 +7,7 @@ export default function Portfolio() {
       <div className='mb-12'>  
         <h3 className='text-3xl font-semibold'>Portfolio</h3>
       </div>
-      <div className='grid grid-cols-1 md:grid-cols-3'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
         {/* render Project component */}
        <Project />
       </div>
